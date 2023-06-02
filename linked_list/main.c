6f8d75165b5a977ecad7e1302515bc55e161ae3e
dpv927 <dpv927@inlumine.ual.es>
2023-06-02 22:07:24 +0200

    Update

--- linked_list/main.c
+++ linked_list/main.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "linkedList.h"
-#define GREATER_THAN 1
-#define LESS_THAN -1
-#define EQUAL_TO 0
-
-int comparer(int* x, int* y) {
-    int xVal = *x;
-    int yVal = *y;
-    return (xVal > yVal) ? GREATER_THAN : ((xVal < yVal) ? LESS_THAN : EQUAL_TO);
-}
 
 int main() {
+    // Create a empty list
+    LinkedList* list = Ll_createNewEmpty();
+    
+    // Add some elements
     int x = 0, y = 1, z = 2;
-    LinkedList* list = Ll_createNewComparable(&comparer);
     Ll_addLast(list, &x);
     Ll_addLast(list, &y);
     Ll_addLast(list, &z);
 
+    // Print the list elements in order using the
+    // LinkedList fields:
     printf("[ ");
     if (list->head != NULL) {
         Node* tmp = list->head;
@@ -29,7 +24,6 @@ int main() {
         }
     }
     printf("]\n\n");
-
     Ll_clear(list);
     free(list);
     return 0;
