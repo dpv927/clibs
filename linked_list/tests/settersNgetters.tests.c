945655dac0e20fdca2c748be0e06774298decb18
dpv927 <dpv927@inlumine.ual.es>
2023-06-14 13:48:44 +0200

    Linked list setters and getters tests

---
+++ linked_list/tests/settersNgetters.tests.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../linkedList.h"
+#include "settersNgetters.tests.h"
+
+void test_Ll_getElementFrom() {
+    // Create a new empty list
+    LinkedList* list = Ll_createNewEmpty();
+
+    int a = 1;
+    int b = 2;
+    int c = 3;
+
+    // Add elements to the list
+    Ll_addLast(list, &a);
+    Ll_addLast(list, &b);
+    Ll_addLast(list, &c);
+
+    // Test getting elements from the list
+    if (*(int*)Ll_getElementFrom(list, 0) != a ||
+        *(int*)Ll_getElementFrom(list, 1) != b ||
+        *(int*)Ll_getElementFrom(list, 2) != c) {
+        printf("Failed test: Ll_getElementFrom() did not return the correct elements.\n");
+        Ll_clear(list);
+        free(list);
+        exit(-1);
+    }
+
+    printf("Successful test: Ll_getElementFrom() returned the correct elements.\n");
+    Ll_clear(list);
+    free(list);
+}
+
+void test_Ll_setElementAt() {
+    // Create a new empty list
+    LinkedList* list = Ll_createNewEmpty();
+
+    int a = 1;
+    int b = 2;
+    int c = 3;
+
+    // Add elements to the list
+    Ll_addLast(list, &a);
+    Ll_addLast(list, &b);
+    Ll_addLast(list, &c);
+
+    int x = 10;
+    int y = 20;
+    int z = 30;
+
+    // Set elements at specific indices in the list
+    Ll_setElementAt(list, 0, &x); // Set at index 0
+    Ll_setElementAt(list, 1, &y); // Set at index 1
+    Ll_setElementAt(list, 2, &z); // Set at index 2
+
+    // Test the updated elements in the list
+    if (*(int*)Ll_getElementFrom(list, 0) != x ||
+        *(int*)Ll_getElementFrom(list, 1) != y ||
+        *(int*)Ll_getElementFrom(list, 2) != z) {
+        printf("Failed test: Ll_setElementAt() did not update the elements correctly.\n");
+        Ll_clear(list);
+        free(list);
+        exit(-1);
+    }
+
+    printf("Successful test: Ll_setElementAt() updated the elements correctly.\n");
+    Ll_clear(list);
+    free(list);
+}
---
+++ linked_list/tests/settersNgetters.tests.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/**
+ * @brief Tests if the function retrieves a element from
+ * a specific index in a linked list.
+ * **/
+void test_Ll_getElementFrom();
+
+/**
+ * @brief Tests if teh function sets a element at a 
+ * specific index in a linked list.
+ * **/
+void test_Ll_setElementAt();
