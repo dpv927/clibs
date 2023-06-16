f0d3dc4690d129ca1cfd4fce04e530187a3da6e1
dpv927 <dpv927@inlumine.ual.es>
2023-06-17 00:36:01 +0200

    Finish first implementation

--- ArrayList/arrayList.c
+++ ArrayList/arrayList.c
@@ -1,24 +1,13 @@
 #include <stdlib.h>
+#include <limits.h>
 #include "arrayList.h"
+
 #define CODE_ERR 1
 #define CODE_GDD 0
 
-ArrayList* Al_createNew() {
-	ArrayList* list = (ArrayList*) malloc(sizeof(ArrayList));
-	if (list == NULL) { return NULL; }
+ArrayList* Al_createNewSized(const uint size) {
+	if (size == 0) { return NULL; }
 
-	list->data = (void**) malloc(sizeof(void*) * DEF_ARL_SIZE);
-	if (list->data == NULL) { 
-		free(list);
-		return NULL; 
-	}
-
-	list->capacity = DEF_ARL_SIZE;
-	list->len = 0;
-	return list;
-}
-
-ArrayList* Al_createNewSized(size_t size) {
 	ArrayList* list = (ArrayList*) malloc(sizeof(ArrayList));
 	if (list == NULL) { return NULL; }
 
@@ -28,23 +17,159 @@ ArrayList* Al_createNewSized(size_t size) {
 		return NULL; 
 	}
 
+	list->comparator = NULL;
 	list->capacity = size;
 	list->len = 0;
 	return list;
 }
 
-void add(ArrayList* list, void* element) {
-
+ArrayList* Al_createNew() {
+	return Al_createNewSized(DEF_ARL_SIZE);
 }
 
-void add(ArrayList* list, void* element, size_t index) {
-
+ArrayList* Al_createNewComparable(const int(*comparer)(void*, void*)) {
+	ArrayList* list = Al_createNewSized(DEF_ARL_SIZE);
+	list->comparator = comparer;
+	return list;
 }
 
-size_t Al_clear(ArrayList* list) {
+ArrayList* Al_createNewComparableSized(const int(*comparer)(void*, void*), const uint size) {
+	ArrayList* list = Al_createNewSized(size);
+	list->comparator = comparer;
+	return list;
+}
+
+Bool Al_add(ArrayList* list, const void* element) {
+	if (Al_ensureCapacity(list)) { return CODE_ERR; }
+	list->data[list->len++] = element;
+	return CODE_GDD;
+}
+
+Bool Al_addAt(ArrayList* list, const void* element, const uint index) {
+	if (index > list->len || Al_ensureCapacity(list)) {
+		return CODE_ERR;
+	}
+
+	for (uint i = list->len - 1; i >= index; i--) {
+		list->data[i + 1] = list->data[i];
+	}
+	list->data[index] = element;
+	list->len++;
+	return CODE_GDD;
+}
+
+void* Al_remove(ArrayList* list, const void* element) {
+	if (list->comparator == NULL) { return NULL; }
+
+	void** removed = NULL;
+	int index = -1;
+
+	for (uint i = 0; i < list->len-1; i++) {
+		if (list->comparator(list->data[i], element) == 0) {
+			removed = list->data[i];
+			index = i;
+			break;
+		}
+	}
+	if (index == -1) { return NULL; } // not found
+	
+	for (uint i = index; i < list->len - 1; i++) {
+		list->data[i] = list->data[i + 1];
+	}
+	list->data[list->len - 1] = NULL;
+	list->len--;
+	return removed;
+}
+
+void* Al_removeFrom(ArrayList* list, const uint index) {
+	if (index < 0 || index > list->len - 1) { return NULL; }
+
+	void* removed = list->data[index];
+	for (uint i = index; i < list->len-1; i++) { 
+		list->data[i] = list->data[i + 1];
+	}
+	list->data[list->len - 1] = NULL;
+	list->len--;
+	return removed;
+}
+
+void* Al_get(const ArrayList* list, const uint index) {
+	if (index < 0 || list->len - 1 > index) { return NULL; }
+	return list->data[index];
+}
+
+int Al_indexOf(const ArrayList* list, const void* element) {
+	if (list->comparator == NULL) { return -1; }
+
+	for (uint i = 0; i < list->len; i++) {
+		if (list->comparator(list->data[i], element) == 0) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+int Al_lastIndexOf(const ArrayList* list, const void* element) {
+	if (list->comparator == NULL) { return -1; }
+
+	int index = -1;
+	for (uint i = 0; i < list->len; i++) {
+		if (list->comparator(list->data[i], element) == 0) {
+			index = i;
+		}
+	}
+	return index;
+}
+
+Bool Al_contains(const ArrayList* list, const void* element) {
+	if (list->comparator == NULL) { return 0; }
+	for (uint i = 0; i < list->len; i++) {
+		if (list->comparator(list->data[i], element) == 0) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+Bool Al_ensureCapacity(ArrayList* list) {
+	if (list->len == list->capacity) {
+		if (list->capacity == INT_MAX) { return CODE_ERR; }
+
+		uint oldLength = list->capacity;
+		uint minGrowth = (list->len + 1) - oldLength;
+		uint prefGrowth = oldLength >> 1;
+
+		uint prefLength = oldLength + (minGrowth > prefGrowth)? minGrowth : prefGrowth;
+		prefLength = (prefLength > INT_MAX) ? INT_MAX : prefLength;
+
+		// Reallocate the old arr at a bigger spot
+		void** new_data = (void**) realloc(list->data, sizeof(void*) * prefLength);
+		if (new_data == NULL) {
+			return CODE_ERR;
+		}
+		list->data = new_data;
+		list->capacity = prefLength;
+	}
+	return CODE_GDD;
+}
+
+Bool Al_trimToSize(ArrayList* list) {
+	if (list->len != list->capacity) {
+		void** new_data = (void**) realloc(list->data, sizeof(void*) * list->len);
+		if (new_data == NULL) {
+			return CODE_ERR;
+		}
+
+		list->data = new_data;
+		list->capacity = list->len;
+	}
+	return CODE_GDD;
+}
+
+Bool Al_clear(ArrayList* list) {
 	if (list == NULL || list->data == NULL) { return CODE_ERR; }
 
-	for (size_t i = 0; i < list->len; i++)
+	for (uint i = 0; i < list->len; i++)
 		free(list->data[i]);
 	list->len = 0;
 
--- ArrayList/arrayList.h
+++ ArrayList/arrayList.h
@@ -2,15 +2,133 @@
 #define DEF_ARL_SIZE 16
 
 typedef struct ArrayList ArrayList;
+typedef unsigned char Bool;
+typedef unsigned int uint;
 
 struct ArrayList {
+	int (*comparator)(void*, void*);
 	void** data;
-	size_t len;
-	size_t capacity;
+	uint len;
+	uint capacity;
 };
 
-ArrayList* Al_createNew();
-ArrayList* Al_createNewSized(size_t size);
-void add(ArrayList* list, void* element);
-void add(ArrayList* list, void* element, size_t index);
-size_t Al_clear(ArrayList* list);
+/**
+ * @brief Creates a new ArrayList with default capacity.
+ *
+ * @return A pointer to the newly created ArrayList. **/
+ArrayList* Al_createNew(void);
+
+/**
+ * @brief Creates a new ArrayList with the specified initial capacity.
+ *
+ * @param size The initial capacity of the ArrayList.
+ * @return A pointer to the newly created ArrayList. **/
+ArrayList* Al_createNewSized(const uint size);
+
+/**
+ * @brief Creates a new ArrayList with the specified element comparer function.
+ *
+ * @param comparer The function used to compare elements in the ArrayList.
+ * @return A pointer to the newly created ArrayList. **/
+ArrayList* Al_createNewComparable(const int(*comparer)(void*, void*));
+
+/**
+ * @brief Creates a new ArrayList with the specified element comparer function and
+ * initial capacity.
+ *
+ * @param comparer The function used to compare elements in the ArrayList.
+ * @param size The initial capacity of the ArrayList.
+ * @return A pointer to the newly created ArrayList. **/
+ArrayList* Al_createNewComparableSized(const int(*comparer)(void*, void*), const uint size);
+
+/**
+ * @brief Adds an element to the end of the ArrayList.
+ *
+ * @param list The ArrayList to which the element will be added.
+ * @param element The element to be added.
+ * @return True if the element was successfully added, False otherwise. **/
+Bool Al_add(ArrayList* list, const void* element);
+
+/**
+ * @brief Adds an element at the specified index in the ArrayList.
+ *
+ * @param list The ArrayList to which the element will be added.
+ * @param element The element to be added.
+ * @param index The index at which the element will be added.
+ * @return True if the element was successfully added, False otherwise. **/
+Bool Al_addAt(ArrayList* list, const void* element, const uint index);
+
+/**
+ * @brief Removes the first occurrence of the specified element from the ArrayList.
+ *
+ * @param list The ArrayList from which the element will be removed.
+ * @param element The element to be removed.
+ * @return A pointer to the removed element, or NULL if the element was not found. **/
+void* Al_remove(ArrayList* list, const void* element);
+
+/**
+ * @brief Removes the element at the specified index from the ArrayList.
+ *
+ * @param list The ArrayList from which the element will be removed.
+ * @param index The index of the element to be removed.
+ * @return A pointer to the removed element, or NULL if the index is out of range. **/
+void* Al_removeFrom(ArrayList* list, const uint index);
+
+/**
+ * @brief Retrieves the element at the specified index from the ArrayList.
+ *
+ * @param list The ArrayList from which the element will be retrieved.
+ * @param index The index of the element to retrieve.
+ * @return A pointer to the element at the specified index, or NULL if the index
+ * is out of range. **/
+void* Al_get(const ArrayList* list, const uint index);
+
+/**
+ * @brief Returns the index of the first occurrence of the specified element in the
+ * ArrayList.
+ *
+ * @param list The ArrayList to search for the element.
+ * @param element The element to search for.
+ * @return The index of the first occurrence of the element, or -1 if the element 
+ * was not found. **/
+int Al_indexOf(const ArrayList* list, const void* element);
+
+/**
+ * @brief Returns the index of the last occurrence of the specified element in the
+ * ArrayList.
+ *
+ * @param list The ArrayList to search for the element.
+ * @param element The element to search for.
+ * @return The index of the last occurrence of the element, or -1 if the element was
+ * not found. **/
+int Al_lastIndexOf(const ArrayList* list, const void* element);
+
+/**
+ * @brief Checks if the ArrayList contains the specified element.
+ *
+ * @param list The ArrayList to check.
+ * @param element The element to check for.
+ * @return True if the element is found in the ArrayList, False otherwise. **/
+Bool Al_contains(const ArrayList* list, const void* element);
+
+/**
+ * @brief Ensures that the ArrayList has enough capacity to accommodate 
+ * additional elements.
+ *
+ * @param list The ArrayList to ensure the capacity for.
+ * @return True if the capacity was successfully ensured, False otherwise. **/
+Bool Al_ensureCapacity(ArrayList* list);
+
+/**
+ * @brief Trims the capacity of the ArrayList to match its current length.
+ *
+ * @param list The ArrayList to trim.
+ * @return True if the ArrayList was successfully trimmed, False otherwise. **/
+Bool Al_trimToSize(ArrayList* list);
+
+/**
+ * @brief Clears the ArrayList, removing all elements.
+ *
+ * @param list The ArrayList to clear.
+ * @return True if the ArrayList was successfully cleared, False otherwise. **/
+Bool Al_clear(ArrayList* list);
